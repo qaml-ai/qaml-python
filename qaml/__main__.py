691931cdadbbb338e813894f6fc2d75df44aedaf
Miguel Salinas <mikeys@me.com>
2024-04-02 14:58:21 -0700

    cli repl

--- qaml/__main__.py
+++ qaml/__main__.py
@@ -1,18 +1,29 @@
 import qaml
 import sys
 import os
+import readline
 
 def main():
-    # get a string of all args
-    args_str = " ".join(sys.argv[1:])
-    print(f"Running command: {args_str}")
     # get api key from environment variable
     api_key = os.environ.get("QAML_API_KEY")
     if api_key is None:
         print("Please set the QAML_API_KEY environment variable")
         sys.exit(1)
+    print("Initializing device driver...")
     client = qaml.Client(api_key=api_key)
-    client.execute(args_str)
+    # if no args, start repl
+    if len(sys.argv) == 1:
+        while True:
+            try:
+                command = input("Enter a command: ")
+                client.execute(command)
+            except EOFError:
+                print("")
+                break
+    else:
+        args_str = " ".join(sys.argv[1:])
+        print(f"Running command: {args_str}")
+        client.execute(args_str)
 
 if __name__ == "__main__":
     main()
