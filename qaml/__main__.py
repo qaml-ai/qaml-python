2475150c318ed16dc51d3c3d87dc4feb05bd5998
Miguel Salinas <mikeys@me.com>
2024-04-02 11:35:36 -0700

    fix cli tool

--- qaml/__main__.py
+++ qaml/__main__.py
@@ -1,10 +1,17 @@
 import qaml
+import sys
+import os
 
 def main():
     # get a string of all args
     args_str = " ".join(sys.argv[1:])
     print(f"Running command: {args_str}")
-    client = qaml.Client()
+    # get api key from environment variable
+    api_key = os.environ.get("QAML_API_KEY")
+    if api_key is None:
+        print("Please set the QAML_API_KEY environment variable")
+        sys.exit(1)
+    client = qaml.Client(api_key=api_key)
     client.execute(args_str)
 
 if __name__ == "__main__":
